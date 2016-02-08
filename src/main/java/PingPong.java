6f90bb4ad655ea94ed0dd39563645d2c51b887bd
Abigail Rolling and Brad London <london.brad@gmail.com>
2016-02-08 11:42:59 -0800

    Create functioning web output

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,11 +1,36 @@
 import java.io.Console;
 import java.util.ArrayList;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 
 public class PingPong {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/pingpong", (request, response) -> {
+      HashMap model = new HashMap();
+      
+      model.put("template", "templates/pingpong.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("pingpongresult", (request, response) -> {
+      HashMap model = new HashMap();
+      String stringLimit = request.queryParams("limitInput");
+      Integer intLimit = Integer.parseInt(stringLimit);
+      ArrayList<Object> pingPongedList = new ArrayList<Object>();
+      pingPongedList = PingPong.pingPongReplacer(intLimit);
+      model.put("result", pingPongedList);
+      model.put("template", "templates/pingpongresult.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
-  public ArrayList<Object> pingPongReplacer(Integer limit) {
+  public static ArrayList<Object> pingPongReplacer(Integer limit) {
     ArrayList<Object> pingPongList = new ArrayList<Object>();
     for (Integer index = 1 ; index <= limit; index ++) {
       if (index % 5 == 0 && index % 3 == 0) {
