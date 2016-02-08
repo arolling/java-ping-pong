1e940a5a07eaa90120358c778db31024d2575ba6
Abigail Rolling and Brad London <london.brad@gmail.com>
2016-02-08 10:14:33 -0800

    Add replacement ping and pong

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -8,7 +8,13 @@ public class PingPong {
   public ArrayList<Object> pingPongReplacer(Integer limit) {
     ArrayList<Object> pingPongList = new ArrayList<Object>();
     for (Integer index = 1 ; index <= limit; index ++) {
-      pingPongList.add(index);
+      if (index % 5 == 0) {
+        pingPongList.add("pong");
+      } else if (index % 3 == 0) {
+        pingPongList.add("ping");
+      } else {
+        pingPongList.add(index);
+      }
     }
     return pingPongList;
   }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -10,7 +10,28 @@ public class PingPongTest {
     ArrayList<Object> testArray = new ArrayList<Object>();
     testArray.add(1);
     testArray.add(2);
-    testArray.add(3);
+    assertEquals(testArray, arrayTest.pingPongReplacer(2));
+  }
+
+  @Test
+  public void pingPongReplacer_replacesMultiplesOfThree_WithPing() {
+    PingPong arrayTest = new PingPong();
+    ArrayList<Object> testArray = new ArrayList<Object>();
+    testArray.add(1);
+    testArray.add(2);
+    testArray.add("ping");
     assertEquals(testArray, arrayTest.pingPongReplacer(3));
+  }
+
+  @Test
+  public void pingPongReplacer_replacesMultiplesOfFive_WithPong() {
+    PingPong arrayTest = new PingPong();
+    ArrayList<Object> testArray = new ArrayList<Object>();
+    testArray.add(1);
+    testArray.add(2);
+    testArray.add("ping");
+    testArray.add(4);
+    testArray.add("pong");
+    assertEquals(testArray, arrayTest.pingPongReplacer(5));
   }
 }
